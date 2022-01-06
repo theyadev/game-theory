1a8ed490ff481ed62229da01bec5d4e291d4a77b
Fassi Théo <gtheya@gmail.com>
2022-01-06 22:10:59 +0100

    Moved class in root dir to a folder named Classes

--- Actions.py
+++
@@ -1,8 +0,0 @@
-class ACTIONS:
-    """
-        Contains Cooperate and Betray as classes variables\n
-        Used in case I need to rename the string names
-    """
-
-    COOPERATE = "Cooperer"
-    BETRAY = "Trahir"
