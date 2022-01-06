ef8cf09bff0ffe357829ed30ba5e1baa4a5f73fd
Fassi Théo <gtheya@gmail.com>
2022-01-06 22:12:28 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import Styles
 
-from Game import Game
+from Classes.Game import Game
 from plot import showGraph
 
 if __name__ == "__main__":
-    newGame = Game(Styles.Betray, Styles.Copy).playEveryRound()
+    newGame = Game(Styles.Test, Styles.Betray).playEveryRound()
 
     showGraph(newGame)
