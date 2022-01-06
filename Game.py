1b8a4fc5dbdb20c6ed7c6db502b64a75152b93e4
Fassi Théo <gtheya@gmail.com>
2022-01-06 19:32:40 +0100

    Added docstring, comments and put points value in settings.py

--- Game.py
+++ Game.py
@@ -1,33 +1,54 @@
 from Player import Player
 from Actions import ACTIONS
 
+from settings import ROUNDS, Points
+
 
 class Game:
+    """
+        Game class, used to run simulation between 2 players.
+
+        Keyword arguments:\n
+        firstPlayerStyle -- takes a Style, define the playstyle of the first player\n
+        secondPlayerStyle -- takes a Style, define the playstyle of the second player
+    """
+
     def __init__(self, firstPlayerStyle, secondPlayerStyle) -> None:
         self.players = (
             Player(0, self, firstPlayerStyle),
             Player(1, self, secondPlayerStyle)
         )
 
-        self.rounds = 5
+        self.rounds = ROUNDS
         self.currentRound = 1
         self.history = []
 
     def step(self):
+        """
+            Step one round into the simulation.\n
+
+            returns None if max round reached
+        """
+
+        if self.currentRound - 1 > self.rounds:
+            return None
+
         self.players[0].action = self.players[0].style.getAction()
         self.players[1].action = self.players[1].style.getAction()
 
+        # TODO: Getting things done DRYer
+
         if self.players[0].action == ACTIONS.COOPERATE and self.players[1].action == ACTIONS.COOPERATE:
-            self.players[0].score += 3
-            self.players[1].score += 3
+            self.players[0].score += Points.BOTH_COOPERATE
+            self.players[1].score += Points.BOTH_COOPERATE
 
         elif self.players[0].action == ACTIONS.BETRAY and self.players[1].action == ACTIONS.BETRAY:
-            self.players[0].score += 1
-            self.players[1].score += 1
+            self.players[0].score += Points.BOTH_BETRAY
+            self.players[1].score += Points.BOTH_BETRAY
 
         else:
-            self.players[0].score += 5 if self.players[0].action == ACTIONS.BETRAY else 0
-            self.players[1].score += 5 if self.players[1].action == ACTIONS.BETRAY else 0
+            self.players[0].score += Points.BETRAY_WHEN_COOPERATED if self.players[0].action == ACTIONS.BETRAY else Points.COOPERATE_WHEN_BETRAYED
+            self.players[1].score += Points.BETRAY_WHEN_COOPERATED if self.players[1].action == ACTIONS.BETRAY else Points.COOPERATE_WHEN_BETRAYED
 
         self.history.append(
             [{"score": player.score, "action": player.action} for player in self.players])
@@ -35,6 +56,10 @@ class Game:
         self.currentRound += 1
 
     def playEveryRound(self):
+        """
+            Runs step function until max round reached.
+        """
+
         while self.currentRound - 1 < self.rounds:
             self.step()
 
---
+++ settings.py
@@ -0,0 +1,13 @@
+ROUNDS = 5
+
+
+class Points:
+    """
+        Contains all points values
+    """
+    
+    BOTH_COOPERATE = 3
+    BOTH_BETRAY = 1
+
+    BETRAY_WHEN_COOPERATED = 5
+    COOPERATE_WHEN_BETRAYED = 0
