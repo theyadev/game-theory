80ee02ae4147e7666799651e3dafd610674fa135
Fassi Théo <gtheya@gmail.com>
2022-01-09 21:46:35 +0100

    Can now show the list of playstyles, userinput

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-import Styles
+from Styles import playstyles
 
 from Classes.Game import Game
 from plot import showGraph
 
-if __name__ == "__main__":
-    newGame = Game(Styles.Betray, Styles.Copy).playEveryRound()
+
+def main():
+    availablePlaystyles = [name for name in playstyles.keys()]
+
+    print(f"List of playstyles: {', '.join(availablePlaystyles)}")
+
+    firstPlaystyle = input("Please choose the first playstyle: ")
+    if not firstPlaystyle in availablePlaystyles:
+        return print("Not a valid playstyle !!!")
+
+    secondPlaystyle = input("Please choose the second playstyle: ")
+    if not secondPlaystyle in availablePlaystyles:
+        return print("Not a valid playstyle !!!")
+
+    newGame = Game(playstyles[firstPlaystyle], playstyles[secondPlaystyle]).playEveryRound()
 
     showGraph(newGame)
+
+
+if __name__ == "__main__":
+    main()
